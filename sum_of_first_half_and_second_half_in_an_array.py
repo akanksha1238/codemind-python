f78e07b77a1aca9cf1a31570c05bbd5ec46e1451
akanksha1238 <84381942+akanksha1238@users.noreply.github.com>
2022-09-23 10:16:28 +0530

    n=int(input()) a=list(map(int,input().split())) h=n//2 s1=s2=0 for i in a:     if i==a[h-1]:         s1+=i         break     else:         s1+=i for i in a:     if i>=a[h]:         s2+=i print(s1) print(s2)

---
+++ sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=list(map(int,input().split()))
+h=n//2
+s1=s2=0
+for i in a:
+    if i==a[h-1]:
+        s1+=i
+        break
+    else:
+        s1+=i
+for i in a:
+    if i>=a[h]:
+        s2+=i
+print(s1)
+print(s2)
