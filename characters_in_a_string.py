3cc2ddf7238a052bccdc6801f1bec51fe5da82ac
akanksha1238 <84381942+akanksha1238@users.noreply.github.com>
2022-10-21 19:26:51 +0530

    s=input() elements=' a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z' count=0 for ch in s:     if ch in elements:         count=count+1 print(count)

---
+++ characters_in_a_string.py
@@ -0,0 +1,7 @@
+s=input()
+elements=' a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'
+count=0
+for ch in s:
+    if ch in elements:
+        count=count+1
+print(count)
