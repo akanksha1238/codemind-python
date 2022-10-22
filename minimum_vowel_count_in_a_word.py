2871ecd96e5ffd4e9494c87db505dfacabfeab2c
akanksha1238 <84381942+akanksha1238@users.noreply.github.com>
2022-10-22 22:19:05 +0530

    s=input() s1=s.split(" ") l=["a","e","i","o","u"] l1=[] ma=1000000 c=0 for i in s1:     c=0     for j in i:         if j in l:             c+=1     if c<ma:         ma=c print(ma)

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,14 @@
+s=input()
+s1=s.split(" ")
+l=["a","e","i","o","u"]
+l1=[]
+ma=1000000
+c=0
+for i in s1:
+    c=0
+    for j in i:
+        if j in l:
+            c+=1
+    if c<ma:
+        ma=c
+print(ma)
