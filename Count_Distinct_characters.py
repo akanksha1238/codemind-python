ca90d6c88c6cfe6c7e4cab23af42d86bdd340d10
akanksha1238 <84381942+akanksha1238@users.noreply.github.com>
2022-10-27 20:26:55 +0530

    s=input() s3=s.replace(" ","") s1=s3.lower() n=len l1=[] for i in s1:     l1.append(i) l1.sort() s3=set(l1) l2=list(s3) l2.sort()
    s=""
    for i in l2:
        s+=i
    print(len(s))

---
+++ Count_Distinct_characters.py
@@ -0,0 +1,16 @@
+s=input()
+s3=s.replace(" ","")
+s1=s3.lower()
+n=len
+l1=[]
+for i in s1:
+    l1.append(i)
+l1.sort()
+s3=set(l1)
+l2=list(s3)
+l2.sort()
+
+s=""
+for i in l2:
+    s+=i
+print(len(s))
